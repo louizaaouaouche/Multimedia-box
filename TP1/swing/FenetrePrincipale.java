8b6ab9671ff8dbb83a12e2e80afb832f3fec25cf
Louiza Aouaouche <louiza.aouaouche@telecom-paris.fr>
2023-02-19 11:27:32 +0100

    update swing

--- TP1/swing/FenetrePrincipale.java
+++ TP1/swing/FenetrePrincipale.java
@@ -1,27 +1,41 @@
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
-public class FenetrePrincipale extends JFrame{
+public class FenetrePrincipale extends JFrame {
     private static final long serialVersionUID = 1L;
 
-    private JButton bouton1;
-    private JButton bouton2;
-    private JButton bouton3;
+    private AbstractAction bouton1;
+    private AbstractAction bouton2;
+    private AbstractAction bouton3;
 
     private JPanel boutonsPanel;
 
-    private JTextArea zoneRequete;
-    private JTextArea zoneReponse;
-    //private JMenuBar barreMenu;
+    private JTextArea zoneTexte;
 
-    private Client client;
+    static final String DEFAULT_HOST = "localhost";
+    static final int DEFAULT_PORT = 3331;
+    static Client client;
 
-    public static void main(String argv[]){
-        FenetrePrincipale fp = new FenetrePrincipale();
+    public static void main(String argv[]) {
+        new FenetrePrincipale();
+        // Java client
+        String host = DEFAULT_HOST;
+        int port = DEFAULT_PORT;
+        if (argv.length >= 1)
+            host = argv[0];
+        if (argv.length >= 2)
+            port = Integer.parseInt(argv[1]);
+
+        // Client client = null;
+
+        try {
+            client = new Client(host, port);
+        } catch (Exception e) {
+            System.err.println("Client: Couldn't connect to " + host + ":" + port);
+            System.exit(1);
+        }
     }
 
     // Constructeur
@@ -35,134 +49,118 @@ public class FenetrePrincipale extends JFrame{
         this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
         // Création des boutons
-        bouton1 = new JButton("Bouton 1");
-        bouton2 = new JButton("Bouton 2");
-        bouton3 = new JButton("Quitter");
+        bouton1 = new ActionSearchMultimedia();
+        bouton2 = new ActionPlay();
+        bouton3 = new ActionExit();
 
         // Création du panel contenant les boutons
         this.boutonsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-        boutonsPanel.add(this.bouton1);
-        boutonsPanel.add(this.bouton2);
-        boutonsPanel.add(this.bouton3);
-
-        // Ajout de listeners aux boutons
-        this.bouton1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                zoneReponse.append("Ajout ligne 1\n");
-            }
-        });
-
-        this.bouton2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                zoneReponse.append("Ajout ligne 2\n");
-            }
-        });
-
-        this.bouton3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                System.exit(0);
-            }
-        });
-
-        // Panel pour les boutons
-        boutonsPanel = new JPanel();
-        boutonsPanel.add(this.bouton1);
-        boutonsPanel.add(this.bouton2);
-        boutonsPanel.add(this.bouton3);
-
+        boutonsPanel.add(new JButton(bouton1));
+        boutonsPanel.add(new JButton(bouton2));
+        boutonsPanel.add(new JButton(bouton3));
 
         // zone de texte pour les requêtes
-        this.zoneRequete = new JTextArea(10, 20);
-        JScrollPane scrollRequete = new JScrollPane(this.zoneRequete);
-        
-        // zone de texte pour les requêtes
-        this.zoneReponse = new JTextArea(10, 20);
-        JScrollPane scrollReponse = new JScrollPane(this.zoneReponse);
-        
+        this.zoneTexte = new JTextArea(10, 20);
+        JScrollPane scrollTexte = new JScrollPane(this.zoneTexte);
+
         // Ajout des éléments à la fenêtre
-        add(scrollRequete, BorderLayout.NORTH);
-        add(scrollReponse, BorderLayout.CENTER);
+        add(scrollTexte, BorderLayout.NORTH);
         add(boutonsPanel, BorderLayout.SOUTH);
-
-        
-        /*this.bouton1 = new JButton("Afficher");
-        this.bouton1.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event ) {
-                String requete = (String)JOptionPane.showInputDialog(FenetrePrincipale.this,"Tapez le nom du media : ",JOptionPane.PLAIN_MESSAGE);
-                String reponse = FenetrePrincipale.this.client.send("printMultimedia" + requete);
-                reponse = reponse.replace("|","\n");
-                zoneReponse.append(reponse+"\n");
-            } 
-        });
-        this.bouton2 = new JButton("Jouer");
-        this.bouton2.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event ) {
-                String requete = (String)JOptionPane.showInputDialog(FenetrePrincipale.this,"Tapez le nom du media : ",JOptionPane.PLAIN_MESSAGE);
-                String reponse = FenetrePrincipale.this.client.send("play" + requete);
-                reponse = reponse.replace("|","\n");
-                zoneReponse.append(reponse+"\n");
-            } 
-        });
-        this.bouton3 = new JButton("Quitter");
-        this.bouton3.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent event ) {
-                System.exit(0);
-                } 
-        });
-        this.boutonsPanel = new JPanel();
-        boutonsPanel.add(this.bouton1, BorderLayout.WEST);
-        boutonsPanel.add(this.bouton2, BorderLayout.CENTER);
-        boutonsPanel.add(this.bouton3, BorderLayout.EAST);
-        add(this.boutonsPanel, BorderLayout.SOUTH);*/
 
         // Création de la barre de menus
         JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        // Création du menu déroulant
         JMenu menu = new JMenu("Menu");
-        JMenuItem menuItem1 = new JMenuItem(new AbstractAction("Menu Item 1") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                zoneReponse.append("Menu Item 1 a été cliqué\n");
-            }
-        });
-
-        JMenuItem menuItem2 = new JMenuItem(new AbstractAction("Menu Item 2") {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                zoneReponse.append("Menu Item 2 a été cliqué\n");
-            }
-        });
-
-        menu.add(menuItem1);
-        menu.add(menuItem2);
-
         menuBar.add(menu);
+
+        // Ajout des actions aux items du menu déroulant
+        JMenuItem item1 = new JMenuItem(new AbstractAction("Rechercher") {
+            public void actionPerformed(ActionEvent e) {
+                zoneTexte.append("Rechercher\n");
+            }
+        });
+        JMenuItem item2 = new JMenuItem(new AbstractAction("Play") {
+            public void actionPerformed(ActionEvent e) {
+                zoneTexte.append("Play\n");
+            }
+        });
+        JMenuItem item3 = new JMenuItem(new AbstractAction("Quitter") {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        menu.add(item1);
+        menu.add(item2);
+        menu.add(item3);
 
         // Création de la barre d'outils
         JToolBar toolBar = new JToolBar();
+        menuBar.add(toolBar, BorderLayout.NORTH);
 
-        toolBar.add(new AbstractAction("Bouton 1", new ImageIcon("icon.png")) {
-            @Override
+        // Ajout des actions à la barre d'outils
+        toolBar.add(new JButton(new AbstractAction("Rechercher") {
             public void actionPerformed(ActionEvent e) {
-                zoneReponse.append("Bouton 1 a été cliqué\n");
+                zoneTexte.append("Rechercher\n");
             }
-        });
+        }));
 
-        toolBar.add(new AbstractAction("Bouton 2", new ImageIcon("icon.png")) {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                zoneReponse.append("Bouton 2 a été cliqué\n");
-            }
-        });
-
-        add(toolBar, BorderLayout.NORTH);
-
-        // Ajout de la barre de menus à la fenêtre principale
-        setJMenuBar(menuBar);
-        
-        
         pack();
         setVisible(true);
 
+    }
+
+    class ActionExit extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+
+        public ActionExit() {
+            this.putValue(AbstractAction.NAME, "Quitter");
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    class ActionPlay extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+
+        public ActionPlay() {
+            this.putValue(AbstractAction.NAME, "Play");
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            String text = zoneTexte.getText();
+            String name = JOptionPane.showInputDialog(null, "Media or group name to play", "Media/Group input",
+                    JOptionPane.QUESTION_MESSAGE);
+
+            try {
+                zoneTexte.setText(text + "play " + name + "\n" + client.send("play " + name) + "\n");
+            } catch (Exception err) {
+                System.err.println("Client: IO erreur");
+            }
+        }
+    }
+
+    class ActionSearchMultimedia extends AbstractAction {
+        private static final long serialVersionUID = 1L;
+
+        public ActionSearchMultimedia() {
+            this.putValue(AbstractAction.NAME, "Recherche");
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            String text = zoneTexte.getText();
+            String name = JOptionPane.showInputDialog(null, "Media or group name to play", "Media/Group input",
+                    JOptionPane.QUESTION_MESSAGE);
+
+            try {
+                zoneTexte.setText(text + "Recherche " + name + "\n" + client.send("searchMultimedia " + name) + "\n");
+            } catch (Exception err) {
+                System.err.println("Client: IO erreur");
+            }
+        }
     }
 
 }
